c1d4c78f5ccaeba4716e2963f6f3d71d11a2bb27
Kanad Gupta <git@kanad.dev>
2025-04-16 14:18:46 -0700

    refactor: ts-node + mocha => tsx + vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    // We'll defer to `@oclif/test` for console interception
+    // so we can run assertions against console output.
+    disableConsoleIntercept: true,
+  },
+})
